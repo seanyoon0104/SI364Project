7428ea8dd59f694ca32f791415328af61ac6cfef
seanyoon0104 <yoonsean@umich.edu>
2019-01-19 16:41:10 +0000

    Models tutorial complete

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ma7r(*&^^o9ycvg0010ss(=spii=ju1tnkoi69a3=ahms-z4(v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
