9a70081fbfed04de7f2849401ef1e755dd92741a
seanyoon0104 <yoonsean@umich.edu>
2019-02-19 21:04:09 +0000

    changed views.py

--- dj4e/home/views.py
+++ dj4e/home/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from dajngo.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+
+def index(request):
+	logging.error("Hello world DJ4E in the log...")
+	print("Hello world DJ4E in a print statment...")
+	response="""<html>
+	<head><meta name=wa4e" content="c9e1074f5b3f9fc8ea15d152add07294"></head>
+	<body><p>Hello world DJ4E in HTML</p>
+	</body></html>"""
+	return HttpResponse(response)
