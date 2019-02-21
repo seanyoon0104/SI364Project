e67402e017060b350ab288451ca13d7187723404
seanyoon0104 <yoonsean@umich.edu>
2019-02-21 20:31:11 +0000

    Updated CRUD assignment but gets 'Add a make' anchor tag missing error

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -19,5 +19,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('autos/', include('autos.urls'))
 ]
--- dj4e/home/views.py
+++ dj4e/home/views.py
@@ -1,19 +1,19 @@
 from django.http import HttpResponse
-import logging
+#import logging
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-def index(request):
-	logging.error("Hello world DJ4E in the log...")
-	print("Hello World DJ4E in a print statement...")
-	response="""<html>
-	<head>
-	<meta name=wa4e content="c9e1074f5b3f9fc8ea15d152add07294">
-	</head>
-	<body>
-	<p>Hello World DJ4E in HTML</p>
-	</body>
-	</html>"""
-	return HttpResponse(response)
+#def index(request):
+#	logging.error("Hello world DJ4E in the log...")
+#	print("Hello World DJ4E in a print statement...")
+#	response=<html>
+#	<head>
+#	<meta name=wa4e content="c9e1074f5b3f9fc8ea15d152add07294">
+#	</head>
+#	<body>
+#	<p>Hello World DJ4E in HTML</p>
+#	</body>
+#	</html>"""
+#	return HttpResponse(response)
